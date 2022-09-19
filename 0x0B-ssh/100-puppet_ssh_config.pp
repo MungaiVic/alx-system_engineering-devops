72306cd0833a41007f838a7512dc26c9a8ec81b2
MungaiVic <victornmungai@gmail.com>
2022-09-19 22:05:27 +0300

    SSH fix 2

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,5 +5,5 @@ file_line { 'Turn off passwd auth':
 }
 file_line { 'Declare identity file':
       path => '/etc/ssh/ssh_config',
-      line => '	IdentityFile ~/.ssh/holberton',
+      line => '	IdentityFile ~/.ssh/school',
 }
